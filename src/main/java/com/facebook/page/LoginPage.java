e2aa7cb03aa025dde1945452a0fa0e4d4b38efe3
Prasanga Fernando <prasangafdz@outlook.com>
2023-11-21 14:47:38 +0530

    Fixed browser navigation issues

--- src/main/java/com/facebook/page/LoginPage.java
+++ src/main/java/com/facebook/page/LoginPage.java
@@ -14,23 +14,37 @@ import java.time.Duration;
 public class LoginPage {
 
     private final By txtUsername =  By.id("email");
-    private final By txtPassword =  By.id("passContainer");
+    private final By txtPassword =  By.id("pass");
+    private final By btnName =  By.name("login");
 
     private static WebDriver driver;
 
-    static {
+//    static {
+//        WebDriverManager.chromedriver().setup();
+//        //  WebDriver driver = new ChromeDriver();
+//
+//        ChromeOptions options = new ChromeOptions();
+////        options.addArguments("--headless");
+//
+//        driver = new ChromeDriver(options);
+//        driver.get("https://web.facebook.com/");
+//    }
+
+    public void setupBrowser(){
         WebDriverManager.chromedriver().setup();
-        //  WebDriver driver = new ChromeDriver();
-
         ChromeOptions options = new ChromeOptions();
-//        options.addArguments("--headless");
-
+//        options.addArguments("--headless");  //Uncomment this to run headless
+        options.addArguments("--remote-allow-origins=*");
         driver = new ChromeDriver(options);
+        driver.get("https://web.facebook.com/");
+        driver.manage().window().maximize();
     }
 
     public void loginWithCredentials(String username, String password){
+        setupBrowser();
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
         wait.until(ExpectedConditions.visibilityOfElementLocated(txtUsername)).sendKeys(username);
         wait.until(ExpectedConditions.visibilityOfElementLocated(txtPassword)).sendKeys(password);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(btnName)).click();
     }
 }
