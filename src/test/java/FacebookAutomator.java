6ea7a5137bf0175caf55996b0e107d49f5b6f876
Prasanga Fernando <prasangafdz@outlook.com>
2023-11-23 15:04:19 +0530

    Added a verification point to the login

--- src/test/java/FacebookAutomator.java
+++ src/test/java/FacebookAutomator.java
@@ -1,7 +1,14 @@
 import com.facebook.function.Login;
+import com.facebook.function.Wall;
+import com.facebook.page.WallPage;
+import org.testng.asserts.SoftAssert;
 
 public class FacebookAutomator {
     public static void main(String[] args) {
-        Login.loginWithCredentials("sss","adfsf");
+        SoftAssert softAsset = new SoftAssert();
+        Login.loginWithCredentials("email","pw");
+        softAsset.assertTrue(Wall.isHomeButtonDisplaying(),"Home button is not displaying");
+        Wall.navigateToMessagesByURL();
+        softAsset.assertAll();
     }
-}
+}//div[@class='x1n2onr6']/div
