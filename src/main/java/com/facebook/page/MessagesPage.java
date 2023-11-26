4bde81d9170740aec2629ce4d25d023f8afa883f
Prasanga Fernando <prasangafdz@outlook.com>
2023-11-26 11:06:57 +0530

    Completed delete for a single conversation

--- src/main/java/com/facebook/page/MessagesPage.java
+++ src/main/java/com/facebook/page/MessagesPage.java
@@ -17,28 +17,33 @@ public class MessagesPage extends LoginPage{
     private List<WebElement> threads;
     private final By btnOptions = By.xpath("//div[@class='x1i64zmx']/div");
     private final By btnDelete = By.xpath("//div[@role='menuitem' and normalize-space()='Delete chat']");
+    private final By lblDeleteAlert = By.xpath("//div[normalize-space()='Delete chat']");
+    private final By btnDeleteOnAlert = By.xpath("//div[@aria-label='Delete chat' and normalize-space()='Delete chat' and not(contains(@aria-disabled,'true'))]");
+
 
     public void gatherNumberOfTreads() throws InterruptedException {
         Thread.sleep(5);
         WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(50));
-//        try{
             wait.until(ExpectedConditions.visibilityOfElementLocated(btnMessage));
             threads = driver.findElements(btnMessage);
             System.out.println(threads.size());
-//        }
-//        catch (Exception e){
-//            wait.until(ExpectedConditions.visibilityOfElementLocated(btnMessage));
-//            threads = driver.findElements(btnMessage);
-//            System.out.println(threads.size());
-//        }
-//        wait.until(ExpectedConditions.visibilityOfElementLocated(btnMessage));
-//        threads = driver.findElements(btnMessage);
-//        System.out.println(threads.size());
+    }
+
+    public void deleteChats(){
         Actions actions = new Actions(driver);
         actions.moveToElement(driver.findElement(btnMessage));
         actions.build().perform();
+        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(50));
         wait.until(ExpectedConditions.visibilityOfElementLocated(btnOptions)).click();
         wait.until(ExpectedConditions.visibilityOfElementLocated(btnDelete)).click();
+        if(isDeleteWarningPopupDisplaying()){
+            wait.until(ExpectedConditions.visibilityOfElementLocated(btnDeleteOnAlert)).click();
+        }
+    }
+
+    public boolean isDeleteWarningPopupDisplaying(){
+        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(50));
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(lblDeleteAlert)).isDisplayed();
     }
 
 }
--- src/test/java/FacebookAutomator.java
+++ src/test/java/FacebookAutomator.java
@@ -5,12 +5,13 @@ import com.facebook.page.WallPage;
 import org.testng.asserts.SoftAssert;
 
 public class FacebookAutomator {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         SoftAssert softAsset = new SoftAssert();
-        Login.loginWithCredentials("","#");
+        Login.loginWithCredentials("","");
         softAsset.assertTrue(Wall.isHomeButtonDisplaying(),"Home button is not displaying");
         Wall.navigateToMessagesByURL();
         Messages.gatherNumberOfTreads();
+        Messages.deleteChats();
         softAsset.assertAll();
     }
 }//div[@class='x1n2onr6']/div
